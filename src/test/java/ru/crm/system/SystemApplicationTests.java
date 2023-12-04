d5fe15b5ebb1ab06eec79b05f5badd5e70594d0b
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2023-12-04 20:05:57 +0300

    Start

---
+++ src/test/java/ru/crm/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.crm.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
